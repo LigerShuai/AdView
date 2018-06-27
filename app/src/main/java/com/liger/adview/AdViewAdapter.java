3e55b33afa661db46e690a64a793ebfcea8a2205
zhengshuai <zhengshuai@xiaomalixing.com>
2018-06-27 12:26:06 +0800

    审核通过前可配置私有仓地址使用

---
+++ app/src/main/java/com/liger/adview/AdViewAdapter.java
@@ -0,0 +1,64 @@
+package com.liger.adview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.liger.adviewlib.AdImageView;
+
+import java.util.List;
+
+/**
+ * @author Liger
+ * @date 2018/6/20 00:12
+ */
+public class AdViewAdapter extends RecyclerView.Adapter<AdViewAdapter.ViewHolder> {
+
+    private List<String> mList;
+
+    public AdViewAdapter(List<String> list) {
+        mList = list;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_adview, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mList.get(position));
+        if (position > 0 && position % 10 == 0) {
+            holder.mTextView.setVisibility(View.INVISIBLE);
+            holder.mImageView.setVisibility(View.INVISIBLE);
+            holder.mAdImageView.setVisibility(View.VISIBLE);
+        } else {
+            holder.mTextView.setVisibility(View.VISIBLE);
+            holder.mImageView.setVisibility(View.VISIBLE);
+            holder.mAdImageView.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mTextView;
+        ImageView mImageView;
+        AdImageView mAdImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextView = itemView.findViewById(R.id.tv);
+            mAdImageView = itemView.findViewById(R.id.item_adview_adimg);
+            mImageView = itemView.findViewById(R.id.iv);
+        }
+    }
+}
--- app/src/main/java/com/liger/adview/MainActivity.java
+++ app/src/main/java/com/liger/adview/MainActivity.java
@@ -1,13 +1,53 @@
 package com.liger.adview;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.liger.adviewlib.AdImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private AdViewAdapter mAdapter;
+    private List<String> mList = new ArrayList<>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.activity_adview_rv);
+
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        for (int i = 0; i < 100; i++) {
+            mList.add(i + " 腾讯科技讯 拥有数亿人口的东南亚，成为中国互联网公司争夺的另外一个舞台，许多有潜力的新创公司成为中国企业投资或并购的目标。" +
+                    "据外媒最新消息，阿里巴巴集团正准备收购印尼的一家互联网金融服务公司。据国外媒体引述多位知情人士的话说，阿里巴巴集团目前正在对印尼" +
+                    "金融产品交易平台 Cermati 展开收购，收购金额可能达到 3000 万美元。");
+        }
+        mAdapter = new AdViewAdapter(mList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int fPos = mLinearLayoutManager.findFirstVisibleItemPosition();
+                int lPos = mLinearLayoutManager.findLastCompletelyVisibleItemPosition();
+                for (int i = fPos; i <= lPos; i++) {
+                    View view = mLinearLayoutManager.findViewByPosition(i);
+                    AdImageView adImageView = view.findViewById(R.id.item_adview_adimg);
+                    if (adImageView.getVisibility() == View.VISIBLE) {
+                        adImageView.setDy(mLinearLayoutManager.getHeight() - view.getTop());
+                    }
+                }
+            }
+        });
     }
 }
